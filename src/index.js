87676c8655d75a640bdd7abda883dca968b33f88
Claudio Magalhães <claudiomagalhaes@eato.com.br>
2021-01-27 19:15:04 -0300

    configurando e criando classe central

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import styles from './styles.module.css'
+import VerifLoader from "./lib/Verify";
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default {
+  VerifLoader
 }
---
+++ src/lib/Verify.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Verify extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>Example Component: {this.props.text}</div>
+    )
+  }
+
+}
+
+export default Verify;
