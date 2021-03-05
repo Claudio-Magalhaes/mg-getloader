5cb65aa77c42b5256811ae554761ddfc0024e897
Claudio Magalhães <claudiomagalhaes@eato.com.br>
2021-03-05 15:50:02 -0300

    incluindo busca de config

--- src/lib/Verify.js
+++ src/lib/Verify.js
@@ -1,44 +1,38 @@
 import React from 'react'
 
-class Verify extends React.Component{
+class Verify extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       loader: true
-    };
+    }
   }
 
   componentDidMount() {
-    if (typeof data.conferiData === "object" && verifyData === false) {
-      setVerifyData(true);
-      if(Object.keys(data.conferiData).length > 0) {
-        setVerifyData(montarVerify(data.conferiData));
+    if (typeof data.conferiData === 'object' && verifyData === false) {
+      setVerifyData(true)
+      if (Object.keys(data.conferiData).length > 0) {
+        setVerifyData(montarVerify(data.conferiData))
       }
 
-      if (typeof data.conferiData === "object" && typeof montarVerify(data.conferiData) === "object") {
-
+      if (
+        typeof data.conferiData === 'object' &&
+        typeof montarVerify(data.conferiData) === 'object'
+      ) {
       }
     }
   }
 
   render() {
-    const {
-      loader
-    } = this.state;
+    const { loader } = this.state
 
     if (loader) {
       return <this.props.Loader />
     } else {
-
-      return (
-        <div>ok</div>
-      )
+      return <div>ok</div>
     }
-    return (
-      <div>Example Component: {this.props.text}</div>
-    )
+    return <div>Example Component: {this.props.text}</div>
   }
-
 }
 
-export default Verify;
+export default Verify
