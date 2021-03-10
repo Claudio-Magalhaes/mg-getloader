7cf74d3372d9ad1a25a686c58811947d200e40ea
Claudio Magalhães <claudiomagalhaes@eato.com.br>
2021-03-10 13:05:43 -0300

    reescrita da lib para incluir novos parametros de entrada e simplificação de código

--- src/index.js
+++ src/index.js
@@ -1,57 +1,117 @@
-import React from 'react'
-import listVerifyData from '../lib/entradas/listRequestData'
-import getData from '../lib/getData'
+import React, { Component, Fragment } from 'react'
+import getData from '../lib/getDate'
+import PropType from 'prop-types'
+import { checkTimerGet, setTmerGet } from '../lib/timerGet'
+require('../src/css/progressBar.css')
+import { checkParam, getDataSession } from "../lib/getDate/config"
+import verifyDataRequest from '../lib/verifyDataRequest'
+import getLog from '../lib/getDate/getLog'
 
-class Index extends React.Component {
+class index extends Component {
   constructor(props) {
     super(props)
     this.state = {
       loader: true,
-      liberado: {
-        config: false,
-        require: false,
-        optional: false
-      },
-      verificado: {}
+      progress: false,
+      progressValue: 0,
+      progressError: '',
+      data: {}
     }
   }
 
   componentDidMount() {
-    const listVerify = listVerifyData(this.props.data)
+    const props = this.props
     new Promise((resolve, reject) => {
-      if (typeof this.props.config === 'object') {
-        if (Object.keys(this.props.config).length >= 1) {
-          Object.keys(this.props.config).map((n) => {
-            getData(
-              n,
-              (result) => {
-                if (Object.values(result).indexOf(false) === -1) {
-                  resolve()
-                } else {
-                  // eslint-disable-next-line prefer-promise-reject-errors
-                  reject()
-                }
-              },
-              this.props.config[n]
-            )
+      if (
+        typeof this.props.config === 'object' ||
+        Array.isArray(this.props.config)
+      ) {
+        // recolhendo dados de busca de paginas
+        const getSiteConfig = Array.isArray(this.props.config)
+          ? this.props.config[0]
+          : this.props.config
+
+        // iniciando configurações de salvamento geral de dados
+        const configSave = {
+          saveLog: this.props.saveLog,
+          rootName: 'siteConfig',
+          timerPause: props.timerGet
+        }
+
+        // alterando dados de salvamento pelo grupo
+        if (this.props.config[1]) {
+          if (typeof this.props.config[1] === 'object') {
+            if (props.config[1].name) {
+              configSave.rootName = props.config[1].name
+            }
+            if (props.config[1]) {
+              configSave.timerPause = props.config[1].timerGet
+            }
+          }
+        }
+
+        if (Object.keys(getSiteConfig).length >= 1) {
+          this.setState({
+            progressValuePart: 100 / Object.keys(getSiteConfig).length
           })
+
+          let self = this
+
+          Object.keys(getSiteConfig).map((n) => {
+            // alterando dados de salvamento individual
+            if (getSiteConfig[n][2]) {
+              if (typeof getSiteConfig[n][2] === 'object') {
+                if (getSiteConfig[n][2].name) {
+                  configSave.rootName = getSiteConfig[n][2].name
+                }
+                if (getSiteConfig[n][2]) {
+                  configSave.timerPause = getSiteConfig[n][2].timerGet
+                }
+              }
+            }
+
+            const name = checkParam(n, getSiteConfig[n][1]).saveName
+
+            if (checkTimerGet(configSave.rootName, name)) {
+              getData(
+                this.props.url,
+                n,
+                this.callback,
+                getSiteConfig[n][0],
+                getSiteConfig[n][1],
+                configSave
+              )
+            } else if (getDataSession(configSave.rootName, name)) {
+              const dataVerify = verifyDataRequest(
+                name,
+                getDataSession(configSave.rootName, name),
+                getSiteConfig[n][0]
+              )
+
+              if (!dataVerify.status) {
+                this.callback(null, null, null, true)
+              }
+
+              this.setState({
+                progressValue:
+                  this.state.progressValue +
+                  100 / Object.keys(getSiteConfig).length
+              })
+            } else {
+              window.alert('ultimo get')
+            }
+          })
+
+          resolve()
         }
       }
 
       resolve()
     })
-      .then(() => {
-        if (listVerify === false) {
-          this.setState({
-            loader: false
-          })
-        } else {
-          this.setState({
-            listVerify
-          })
-        }
+      .then((name, data, root) => {
       })
-      .catch(() => {
+      .catch((e) => {
+        console.log(e)
         console.error(
           'erro de busca:',
           'não foi possível buscar dados de configuração do site'
@@ -59,117 +119,82 @@ class Index extends React.Component {
       })
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (
-      typeof prevState.listVerify === 'undefined' &&
-      typeof this.state.listVerify === 'object'
-    ) {
-      this.configGetData()
-    }
-
-    if (
-      typeof this.state.listVerify === 'object' &&
-      this.state.loader === true &&
-      Object.values(this.state.liberado).indexOf(false) === -1 &&
-      this.state.liberado !== prevState.liberado
-    ) {
-      this.verifyDisableLoader()
-    }
-
-    if (
-      Object.keys(this.state.liberado).length ===
-        Object.keys(this.state.verificado).length &&
-      this.state.loader
-    ) {
-      if (Object.values(this.state.verificado).indexOf(false) === -1) {
-        // eslint-disable-next-line react/no-did-update-set-state
-        this.setState({
-          loader: false
-        })
-      }
-    }
-  }
-
-  loop = (c, listVerify, push = false) => {
-    let log = []
-    if (typeof listVerify[c] === 'boolean') {
-      return [listVerify[c]]
-    } else if (typeof listVerify[c] === 'object') {
-      Object.keys(listVerify[c]).map((v, k, b, p = push) => {
-        if (p) {
-          log.concat(...this.loop(v, listVerify[c], true))
-        } else {
-          log.push(...this.loop(v, listVerify[c], true))
-        }
-        log = [...log, ...this.loop(v, listVerify[c], true)]
-      })
-    }
-    return log
-  }
-
-  verifyDisableLoader = () => {
-    const objVerify = {}
-
-    Object.keys(this.state.liberado).map((v) => {
-      objVerify[v] = this.loop(v, this.state.listVerify).indexOf(false) === -1
-    })
-
+  callback = (name, data, nivel, error = false) => {
     this.setState({
-      verificado: objVerify
+      progressValue: this.state.progressValue + this.state.progressValuePart
     })
-  }
-
-  configGetData = () => {
-    const list = this.state.listVerify
-
-    const dataRouter = this.props.data.data
-
-    Object.keys(list).map((l) => {
-      if (typeof list[l] === 'object') {
-        if (Object.keys(list[l]).length > 0) {
-          Object.keys(list[l]).map((n) => {
-            getData(
-              n,
-              (p) => {
-                this.setState({
-                  listVerify: {
-                    ...this.state.listVerify,
-                    [l]: {
-                      ...this.state.listVerify[l],
-                      [n]: p
-                    }
-                  },
-                  liberado: {
-                    ...this.state.liberado,
-                    [l]: true
-                  }
-                })
-              },
-              dataRouter[l][n]
-            )
-          })
-          return
-        }
-      }
-
-      const liberado = this.state.liberado
-      delete liberado[l]
-
+    console.log(name)
+    console.log(data)
+    console.log(nivel)
+    if (error) {
       this.setState({
-        liberado: liberado
+        progressError: 'error'
       })
-    })
+    } else {
+      this.setState({
+        data: {
+          ...data,
+          [nivel]: {
+            ...data[nivel],
+            [name]: data
+          }
+        },
+        progressValue: this.state.progressValue + this.state.progressValuePart
+      })
+    }
   }
 
   render() {
-    const { loader } = this.state
+    const { loader, progressValue, progressError } = this.state
 
-    if (loader) {
-      return this.props.Loader
-    } else {
-      return <this.props.data.Page {...this.props} />
+    if (!loader) {
+      return (
+        <this.props.data.Page
+          {...this.props}
+          loaderOn={() => this.setState({ loader: true })}
+          loaderOff={() => this.setState({ loader: false })}
+        />
+      )
     }
+
+    return (
+      <Fragment>
+        {this.props.Loader}
+        <div className='pre-progress'>
+          <div className='progress'>
+            <div
+              className={`progress-value ${progressError}`}
+              style={{ width: `${progressValue}%` }}
+            />
+          </div>
+        </div>
+      </Fragment>
+    )
   }
 }
 
-export default Index
+index.defaultProps = {
+  config: {},
+  timerGet: 3,
+  url: {
+    base:
+      window.location.origin + '/core_magales/Public/Functions/DataSite?page=',
+    alternative:
+      window.location.origin + '/core_magales/Public/Functions/DataSite?page=',
+    test:
+      window.location.origin +
+      '/core_magales/Public/Functions/DataSite/previa?page='
+  },
+  permiteErro: false,
+  saveLog: true
+}
+
+index.propTypes = {
+  config: PropType.object,
+  timerGet: PropType.number,
+  baseUrl: PropType.object,
+  alternativeUrl: PropType.string,
+  TestUrl: PropType.string
+}
+
+export default index
